1e8f7477f4b6ace465911751dd83dd462219cfd6
janjao937 <janjao937@gmail.com>
2023-10-22 14:33:48 +0700

    cartContext_createCartItem-increseCartItem |connect api

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -5,55 +5,108 @@ import myAxios from "../config/myAxios";
 const cartContext = React.createContext();
 
 const CartContextProvider = ({children})=>{
-    const [cartItems,setCartItems] = useState([]);
+    const [cartItems,setCartItems] = useState([]);//uesEffect get all cart item
     const [totalPrice,setTotalPrice] = useState(null);
 
-    useEffect(()=>{
-        Total();
+    // useEffect(()=>{
+    //     if(totalPrice){
 
-    },cartItems);
+    //         Total();
+    //     }
 
-    
-    const AddProduct = (product) =>{
+    // },cartItems);
+
+    // //call create or increse
+    // const AddProductResponse = ()=>{
+
+    //     // myAxios.post//create new item
+
+    //     // myAxios.patch //add amount
+    // }
+    // //call Delete or remove
+    // const RemoveProductResponse = ()=>{
+
+    // }
+
+    const createCart = async(product)=>{
+        // console.log(product)
+        const res = await myAxios.post("/cart/create",{
+            productId:product.id,
+            isOrderStatus:0,
+            amount:1
+        });
+
+        // console.log("create cart"+res);
+        return res;
+
+    }
+
+    const IncreseAmout = async (product)=>{
+        console.log(product);
+        const res = await myAxios.patch("/cart/add",{
+            productId:product.id,
+        });
+        console.log(res);
+    }
+    const DecreseAmout = async (product)=>{
+        console.log(product);
+        const res = await myAxios.patch("/cart/remove",{
+            productId:product.id,
+        });
+        console.log(res);
+    }
+    const DeleteCart = async(product)=>{
+        const res = await myAxios.delete("/cart/delete",{
+            productId:product.id
+        });
+        return res;
+    }
+
+
+
+    const AddProduct = async(product) =>{
         //add item to cart 
         const productExist = cartItems.find((item)=>item.id === product.id);
         if(productExist){
             setCartItems(cartItems.map((item)=>item.id === product.id?{...productExist,quantity:productExist.quantity+1}:item));
+            //patch
+           
+            const res = await IncreseAmout(product);
         }
         else{
             setCartItems([...cartItems,{...product,quantity:1}]);
+            //create
+            const res = await createCart(product);
         }
     }
-    //call create or increse
-    const AddProductResponse = ()=>{
 
-        // myAxios.post//create new item
 
-        // myAxios.patch //add amount
-    }
-    //call Delete or remove
-    const RemoveProductResponse = ()=>{
-
-    }
-
-    const RemoveProduct = (product)=>{
+    const RemoveProduct = async(product)=>{
          //remove item in cart 
          const productExist = cartItems.find((item)=>item.id === product.id);
          if(productExist.quantity === 1){
             setCartItems(cartItems.filter((item)=>item.id !== product.id));
+            //delete
+            // const res = await DeleteCart(product);
          }
          else{
             setCartItems(cartItems.map((item)=>item.id === product.id?{...productExist,quantity:productExist.quantity-1}:item));
+            //patch
+            // const res = await DecreseAmout(product);
          }
     }
 
     const Total = ()=>{
         let initialValue = 0;
         //reduce cart Item
-         const total =cartItems.reduce(e=>e.price*e.quantity,initialValue);
+         const total =cartItems?.reduce(e=>e.price*e.quantity,initialValue);
          setTotalPrice(total);//set state totalPrice
          console.log(totalPrice);
          
+    }
+
+    const CreateOrder = ()=>{
+
     }
 
     return(
